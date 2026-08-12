183c78795e372f7f209d76272bf1e302608b461c
ShaliniYS30 <shalinishalini5768@gmail.com>
2026-08-12 23:08:42 +0530

    Add missing number

---
+++ 02_Arrays/Missing Number.cpp
@@ -0,0 +1,17 @@
+//Problem:Missing Number From  1 to N
+//Platform:leetcode
+//difficulty:easy
+//Language:c++
+class Solution{
+    public:
+    int missingNumber(vector<int>& nums){
+        int xor1=0;
+        int xor2=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            xor1^=(i+1);
+            xor2^=nums[i];
+        }
+        return xor1^xor2;
+    }
+}
