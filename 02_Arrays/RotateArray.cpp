4db7f26607fa75365872e3481ee30443c133c786
ShaliniYS30 <shalinishalini5768@gmail.com>
2026-08-14 22:25:30 +0530

    Add Maximum Consecutive ones

---
+++ 02_Arrays/RotateArray.cpp
@@ -0,0 +1,14 @@
+//Problem:Rotate array by D places
+//Platform:Geeksforgeeks
+//difficulty:medium
+//Language:c++
+class Solution{
+    public:
+    void rotateArr(vector<int> &arr, int d){
+    int n= arr.size();
+    d=d%n;
+    reverse(arr.begin(),arr.begin()+d):
+    reverse(arr.begin()+d,arr.end());
+    reverse(arr.begin(),arr.end())
+    }
+};
