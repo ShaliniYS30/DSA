29f0d8fb710ae607b7d84ce0cbf92709cf1fbd0b
ShaliniYS30 <shalinishalini5768@gmail.com>
2026-09-11 22:24:44 +0530

    Added Search in Rotated Sorted Array 02

---
+++ 03_BinarySearch/Search_in_Rotated_Sorted_Array_02.cpp
@@ -0,0 +1,55 @@
+//Problem:Search in Rotated Sorted Array_02
+//Platform:Leetcode
+//Difficulty:Medium
+//Language:c++
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        while(low<=high){
+           int mid=low+(high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+                if(nums[low]==nums[mid]&&nums[mid]==nums[high]){
+                    low++;
+                    high--;
+                    continue;
+                }
+             if(nums[low]<=nums[mid]){
+            if(nums[low]<=target&&target<nums[mid]){
+                high=mid-1;
+                }
+                else{
+                    low=mid+1;
+ }
+            }
+            else
+            if(nums[mid]<target&&target<=nums[high]){
+                low=mid+1;
+                }
+                else{
+                high=mid-1;
+
+                }
+        }
+        return -1;
+    }
+};
+int main() {
+
+    vector<int> nums = {4,4, 5, 8,6, 7, 0, 1, 2};
+    int target = 9;
+
+    Solution obj;
+
+    int ans = obj.search(nums, target);
+
+    cout << ans;
+
+    return 0;
+}
